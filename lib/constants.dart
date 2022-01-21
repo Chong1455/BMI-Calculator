1883ef4eda0c5d5d32ff8b7bd280a36fdf01433d
Chong Kok Sian <ckh_5316@yahoo.com>
2022-01-21 08:42:02 +0800

    final commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,6 +10,31 @@ const kNumberTextStyle = TextStyle(
   fontWeight: FontWeight.w900,
 );
 
+const KLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
+
 const kBottomContainerHeight = 80.0;
 const kActiveCardColour = Color(0XFF1D1E33);
 const kInactiveCardColour = Color(0XFF111328);
